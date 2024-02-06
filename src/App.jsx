2f67c746bcc49a46d4d4da1bd807bda209893a09
legodev <gonzalezleonardo283@gmail.com>
2024-02-06 18:28:12 -0300

    Rendering array.map

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import './App.css'
 
-function App() {
+const data = [
+  {
+    id: '1',
+    title: 'Tiramisu',
+    description: 'The best tiramisu in town',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$4.50',
+  },
+  {
+    id: '2',
+    title: 'Popcorn',
+    description: 'The best popcorn in town',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$2.60',
+  },
+  {
+    id: '3',
+    title: 'Ceviche',
+    description: 'The best ceviche in town',
+    image: 'https://picsum.photos/200/300/?random',
+    price: '$8.00',
+  },
+]
 
+function App() {
+  const topDesserts = data.map((dessert) => {
+    return (
+      {
+        content: `${dessert.title} - ${dessert.description}`,
+        price: dessert.price,
+        id: dessert.id
+      }
+    )
+  })
+
+  const rend = topDesserts.map(dessert => {
+    return       (
+      <div key={dessert.id}>
+        <p>{dessert.content}</p>
+        <p>{dessert.price}</p>
+      </div>
+    )
+  })
+  console.log(topDesserts)
   return (
     <>
-      <h1>Hello React</h1>
+      <div>{rend}</div>
     </>
   )
 }
