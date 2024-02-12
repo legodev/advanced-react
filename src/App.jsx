d8f260b5a6e92f085e9e5426295d80aafbe2d5ab
legodev <gonzalezleonardo283@gmail.com>
2024-02-11 23:50:02 -0300

    Hello React

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import './App.css'
-import ThemeProvider from './components/ThemeProvider'
-import { ThemeContext, useTheme } from './hooks/useTheme'
+// import ThemeProvider from './components/ThemeProvider'
+// import { ThemeContext, useTheme } from './hooks/useTheme'
 
 function App() {
-  function Cases() {
-    const level = useTheme(ThemeContext)
-    switch (level) {
-      case 1:
-        return <h1>Caso 1</h1>
-      case 2:
-        return <h6>Caso 2</h6>
-      default:
-        return <h2>Sin un valor válido</h2>
-    }
-  }
+  // function Cases() {
+  //   const level = useTheme(ThemeContext)
+  //   switch (level) {
+  //     case 1:
+  //       return <h1>Caso 1</h1>
+  //     case 2:
+  //       return <h6>Caso 2</h6>
+  //     default:
+  //       return <h2>Sin un valor válido</h2>
+  //   }
+  // }
 
   return (
-    <ThemeProvider value={0}>
-      <Cases></Cases>
-    </ThemeProvider>
+    // <ThemeProvider value={0}>
+    //   <Cases></Cases>
+    // </ThemeProvider>
+    <h1>Hello React</h1>
   )
 }
 export default App
--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,7 +2,9 @@ import { useContext } from "react";
 import { createContext } from "react";
 
 export const ThemeContext = createContext(undefined)
+//Create context
 
 export const useTheme = () => {
     return useContext(ThemeContext)
 }
+// Use context
