ec670908807eb24051fb4ffd53bd84c8de579752
legodev <gonzalezleonardo283@gmail.com>
2024-02-16 19:18:31 -0300

    Object.keys

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 export default function App() {
   const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
+  // const [loading, setLoading] = useState(true)
 
   async function request() {
     try {
@@ -15,18 +15,30 @@ export default function App() {
       setData(data.results[0])
     } catch (error) {
       console.log(error)
-    } finally {
-      setLoading(false)
     }
   }
+
+  const keys = Object.entries(data)
+  console.log(keys)
+
+  const matriz = [
+    [1, 2, 3],
+    [4, [77, 11, [5], 99], 6],
+    [7, 8, 9],
+  ]
+  const five = matriz[1][1][2][0]
+  console.log(five)
 
   console.log(data)
   useEffect(() => {
     request()
   }, [])
-  return (
+  return Object.keys(data).length > 0 ? (
     <div style={{ padding: '40px' }}>
-      {loading ? <p>Data fetching</p> : `${data.gender}`}
+      {/* {loading ? <p>Data fetching</p> : `${data.gender}`} */}
+      <p>{data.gender}</p>
     </div>
+  ) : (
+    <p>Data fetching</p>
   )
 }
