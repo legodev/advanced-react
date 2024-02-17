a4b996ecc299c0028054c84b2490a25372c69f3b
legodev <gonzalezleonardo283@gmail.com>
2024-02-16 23:34:12 -0300

    Updating project

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,75 @@
-import React from 'react'
+// import React from 'react'
+
+// function App() {
+//   const [user, setUser] = React.useState([])
+
+//   const fetchData = () => {
+//     try {
+//       fetch('https://randomuser.me/api/?results=1')
+//         .then((res) => res.json())
+//         .then((data) => setUser(data.results[0]))
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+//   console.log(user)
+
+//   React.useEffect(() => {
+//     fetchData()
+//   }, [])
+
+//   return Object.keys(user).length > 0 ? (
+//     <div style={{ padding: '40px' }}>
+//       <h1>Customer data</h1>
+//       <div>
+//         <h2>
+//           {user.name.first} {user.name.last}
+//         </h2>
+//         <picture>
+//           <img
+//             src={user.picture.large}
+//             width={200}
+//             alt={`${user.name.first} ${user.name.last} Image`}
+//           />
+//         </picture>
+//       </div>
+//     </div>
+//   ) : (
+//     <h1>Data pending...</h1>
+//   )
+// }
+
+// export default App
+
+import React from "react";
 
 function App() {
-  const [user, setUser] = React.useState([])
+  const [user, setUser] = React.useState([]);
 
   const fetchData = () => {
-    try {
-      fetch('https://randomuser.me/api/?results=1')
-        .then((res) => res.json())
-        .then((data) => setUser(data.results[0]))
-    } catch (error) {
-      console.log(error)
-    }
-  }
+    fetch('https://randomuser.me/api/?results=1')
+      .then(res => res.json())
+      .then(data => setUser(data.results[0]))
+  };
 
   console.log(user)
 
   React.useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return Object.keys(user).length > 0 ? (
-    <div style={{ padding: '40px' }}>
+    <div style={{padding: "40px"}}>
       <h1>Customer data</h1>
       <div>
-        <h2>
-          {user.name.first} {user.name.last}
-        </h2>
-        <picture>
-          <img
-            src={user.picture.large}
-            width={200}
-            alt={`${user.name.first} ${user.name.last} Image`}
-          />
-        </picture>
+        <h2>{user.name.first} {user.name.last}</h2>
+        <img src={user.picture.large} />
       </div>
     </div>
   ) : (
     <h1>Data pending...</h1>
-  )
+  );
 }
 
-export default App
+export default App;
