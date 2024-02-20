6d18c1b6aeba0e7f58194d257b224e05ed60ba50
legodev <gonzalezleonardo283@gmail.com>
2024-02-19 22:43:04 -0300

    UseReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,32 @@
-// import React from 'react'
+import { useReducer, useState } from 'react'
 
-// function App() {
-//   const [user, setUser] = React.useState([])
+// action = {type: string, payload: any}
 
-//   const fetchData = () => {
-//     try {
-//       fetch('https://randomuser.me/api/?results=1')
-//         .then((res) => res.json())
-//         .then((data) => setUser(data.results[0]))
-//     } catch (error) {
-//       console.log(error)
-//     }
-//   }
 
-//   console.log(user)
+export default function App() {
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'add':
+        return { counter: state.counter + 10 }
+      case 'remove':
+        return { counter: state.counter - 10 }
+      case 'set':
+        return {counter: action.payload}
+      default:
+        return state
+    }
+  }
+  const initialValue = { counter: 100 }
 
-//   React.useEffect(() => {
-//     fetchData()
-//   }, [])
+  const [state, dispatch] = useReducer(reducer, initialValue)
+  console.log(state)
 
-//   return Object.keys(user).length > 0 ? (
-//     <div style={{ padding: '40px' }}>
-//       <h1>Customer data</h1>
-//       <div>
-//         <h2>
-//           {user.name.first} {user.name.last}
-//         </h2>
-//         <picture>
-//           <img
-//             src={user.picture.large}
-//             width={200}
-//             alt={`${user.name.first} ${user.name.last} Image`}
-//           />
-//         </picture>
-//       </div>
-//     </div>
-//   ) : (
-//     <h1>Data pending...</h1>
-//   )
-// }
-
-// export default App
-
-import React from "react";
-
-function App() {
-  const [user, setUser] = React.useState([]);
-
-  const fetchData = () => {
-    fetch('https://randomuser.me/api/?results=1')
-      .then(res => res.json())
-      .then(data => setUser(data.results[0]))
-  };
-
-  console.log(user)
-
-  React.useEffect(() => {
-    fetchData();
-  }, []);
-
-  return Object.keys(user).length > 0 ? (
-    <div style={{padding: "40px"}}>
-      <h1>Customer data</h1>
-      <div>
-        <h2>{user.name.first} {user.name.last}</h2>
-        <img src={user.picture.large} />
-      </div>
+  return (
+    <div>
+      <h1>{state.counter}</h1>
+      <button onClick={() => dispatch({ type: 'add' })}>Add</button>
+      <button onClick={() => dispatch({ type: 'remove' })}>Remove</button>
+      <button onClick={() => dispatch({ type: 'set', payload: 100 })}>Set</button>
     </div>
-  ) : (
-    <h1>Data pending...</h1>
-  );
+  )
 }
-
-export default App;
