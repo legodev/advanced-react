2d32d4b7199693a5cb50261eb77d18170d298465
legodev <gonzalezleonardo283@gmail.com>
2024-02-20 03:48:14 -0300

    UseRef from Meta

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useReducer} from 'react'
-
-// action = {type: string, payload: any}
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'add':
-      return { counter: state.counter + 10 }
-    case 'remove':
-      return { counter: state.counter - 10 }
-    case 'set':
-      return { counter: action.payload }
-    default:
-      return state
-  }
-}
+import { useRef } from "react"
 
 export default function App() {
-  const initialValue = { counter: 100 }
 
-  const [state, dispatch] = useReducer(reducer, initialValue)
-  console.log(state)
+const inputRef = useRef()
 
-  const set = 0
+const inputFocus = () => {
+  inputRef.current.focus()
+}
 
   return (
-    <div>
-      <h1>{state.counter}</h1>
-      <button onClick={() => dispatch({ type: 'add' })}>Add</button>
-      <button onClick={() => dispatch({ type: 'remove' })}>Remove</button>
-      <button onClick={() => dispatch({ type: 'set', payload: set })}>
-        Set
-      </button>
-    </div>
+    <>
+      <h1>Form</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={inputFocus}>Click Me!</button>
+    </>
   )
 }
