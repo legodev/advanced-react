804be17cc5adb434ca75982b9b77fba7e07186b3
legodev <gonzalezleonardo283@gmail.com>
2024-02-22 16:42:48 -0300

    Custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-import { useRef } from "react"
-
+import { useState, useEffect, useRef } from "react";
 export default function App() {
-
-const inputRef = useRef()
-
-const inputFocus = () => {
-  inputRef.current.focus()
-}
-
+  const [day, setDay] = useState("Monday");
+  const prevDay = usePrevious(day);
+  const getNextDay = () => {
+    if (day === "Monday") {
+      setDay("Tuesday")
+    } else if (day === "Tuesday") {
+      setDay("Wednesday")
+    } else if (day === "Wednesday") {
+      setDay("Thursday")
+    } else if (day === "Thursday") {
+      setDay("Friday")
+    } else if (day === "Friday") {
+      setDay("Monday")
+    }
+  }
   return (
-    <>
-      <h1>Form</h1>
-      <input type="text" ref={inputRef} />
-      <button onClick={inputFocus}>Click Me!</button>
-    </>
-  )
+    <div style={{padding: "40px"}}>
+      <h1>
+        Today is: {day}<br />
+        {
+          prevDay && (
+            <span>Previous work day was: {prevDay}</span>
+          )
+        }
+      </h1>
+      <button onClick={getNextDay}>
+        Get next day
+      </button>
+    </div>
+  );
+}
+function usePrevious(val) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = val;
+  }, [val]);
+  return ref.current;
 }
