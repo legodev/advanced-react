f4f92553a7ab033c9bda5d7eec72007a94803ff6
legodev <gonzalezleonardo283@gmail.com>
2024-02-28 05:46:49 -0300

    Render props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,48 @@
+import { useState, useEffect } from 'react'
+
+const DataFetcher = ({ render, url }) => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    if (url.includes('desserts')) {
+      setData(['cake', 'ice cream', 'pie', 'brownie'])
+    } else {
+      setData(['water', 'soda', 'juice'])
+    }
+  }, [])
+
+  return render(data)
+}
+
+const DessertsCount = () => {
+  return (
+    <DataFetcher
+      url="https://littlelemon/desserts"
+      render={(data) => {
+        return <p>{data.length} desserts</p>
+      }}
+    />
+  )
+}
+
+const DrinksCount = () => {
+  return (
+    <DataFetcher
+    url="https://littlelemon/drinks"
+    render={(data) => {
+      return (
+        <h3>{data.length} drinks</h3>
+      )
+    }} />
+  )
+}
+
 export default function App() {
-  return(
+  return (
     <>
       <h1>Hello React</h1>
-      <p>New Project...</p>
+      <DessertsCount />
+      <DrinksCount />
     </>
   )
 }
