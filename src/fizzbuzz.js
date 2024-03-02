d9f95a408ee42bd9077e3b13ec48231b29fe60a8
legodev <gonzalezleonardo283@gmail.com>
2024-03-02 11:38:23 -0300

    First test

---
+++ src/fizzbuzz.js
@@ -0,0 +1,11 @@
+export const fizzbuzz = (number) => {
+    if (typeof number !== 'number') throw new Error()
+
+    
+  
+    if (number % 5 === 0 && number % 3 === 0) return 'fizzbuzz'
+    if (number % 3 === 0) return 'fizz'
+    if (number % 5 === 0) return 'buzz'
+  
+    return number
+  }
---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest'
+import { fizzbuzz } from '../src/fizzbuzz'
+
+
+describe('fizzbuzz', () => {
+  //   it('should be a function')
+  //   expect(typeof fizzbuzz).toBe('function')
+
+  it('should return 2 if number provided is 2', () => {
+    expect(fizzbuzz(2)).toBe(2)
+  })
+
+  it('should return "fizz" if number provided is multiple of 3', () => {
+    expect(fizzbuzz(6)).toBe('fizz')
+  })
+
+  it('parameter provided should be a number', () => {
+    expect(() => fizzbuzz()).toThrow()
+  })
+
+  it('should return "buzz" if number is multiple of 5', () => {
+    expect(fizzbuzz(10)).toBe('buzz')
+    expect(fizzbuzz(20)).toBe('buzz')
+  })
+
+  it('should return "fizzbuzz" if number is multiple of 3 and 5', () => {
+    expect(fizzbuzz(15)).toBe('fizzbuzz')
+  })
+})
