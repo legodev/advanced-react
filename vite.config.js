fb4a4db12ac80679a9d810178d618713c75945b8
legodev <gonzalezleonardo283@gmail.com>
2024-03-06 18:16:27 -0300

    New configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test : {
-    environment: 'happy-dom',
-    globals: true,
-  }  
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+    }
+  }
+  
 })
