cc07a14c3a842a7af3f2c00958eb5e285924c08c
legodev <gonzalezleonardo283@gmail.com>
2024-03-06 18:16:17 -0300

    New configuration

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,16 +1,31 @@
 import { cleanup, render, screen } from '@testing-library/react'
-import { afterEach, describe, it } from 'vitest'
+import { afterEach, describe, it, expect } from 'vitest'
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+const rows = [
+  [7, 8, 9],
+  [4, 5, 6],
+  [1, 2, 3],
+  [0]
+]
+
+const operations = ['+', '-', '*', '/']
+
 const Calculator = () => {
   return (
-    <>
+    <section>
       <h1>Calculator</h1>
-      {numbers.map((number) => (
-        <span key={number}>{number}</span>
-      ))}
-    </>
+      <div role="grid">
+        {rows.map((row, idx) => (
+          <div role='row' key={idx}>
+            {row.map((number) => (
+              <span key={number}>{number}</span>
+            ))}
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
@@ -35,5 +50,14 @@ describe('Calculator', () => {
 
   it('should render 4 rows', () => {
     render(<Calculator />)
+    const rows = screen.getAllByRole('row')
+    expect(rows).toHaveLength(4)
+  })
+
+  it('should render operations', () => {
+    render(<Calculator />)
+    operations.forEach(operation => {
+      screen.getByText(operation)
+    })
   })
 })
