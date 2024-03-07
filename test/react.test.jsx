71e0a38c986a04464e772b2307a6a962f230a379
legodev <gonzalezleonardo283@gmail.com>
2024-03-07 14:33:59 -0300

    More testing

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,4 +1,5 @@
-import { cleanup, render, screen } from '@testing-library/react'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
+import { useState } from 'react'
 import { afterEach, describe, it, expect } from 'vitest'
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -11,19 +12,31 @@ const rows = [
 ]
 
 const operations = ['+', '-', '*', '/']
+const equalSign = '='
+
+
 
 const Calculator = () => {
+  const [value, setValue] = useState('')
+
+  const createHandleClick = (op) => () => setValue(value.concat(op))
+
   return (
     <section>
       <h1>Calculator</h1>
+      <input value={value} readOnly/>
       <div role="grid">
         {rows.map((row, idx) => (
           <div role='row' key={idx}>
             {row.map((number) => (
-              <span key={number}>{number}</span>
+              <button onClick={createHandleClick(number)} key={number}>{number}</button>
             ))}
           </div>
         ))}
+        {operations.map(operation => (
+          <button onClick={createHandleClick(operation)} key={operation}>{operation}</button>
+        ))}
+        <button>{equalSign}</button>
       </div>
     </section>
   )
@@ -60,4 +73,72 @@ describe('Calculator', () => {
       screen.getByText(operation)
     })
   })
+
+  it('should render equal sign', () => {
+    render(<Calculator />)
+    screen.getByText('=')
+  })
+
+  it('should render an input', () => {
+    render(<Calculator />)
+    screen.getByRole('textbox')
+  })
+
+  it('should user input after clicking a number', () => {
+    render(<Calculator />)
+    
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+
+    const input = screen.getByRole('textbox')
+    expect(input.value).toBe('1')
+  })
+
+  it('should user input after clicking a several numbers', () => {
+    render(<Calculator />)
+    
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+
+    const two = screen.getByText('2')
+    fireEvent.click(two)
+
+    const three = screen.getByText('3')
+    fireEvent.click(three)
+
+    const input = screen.getByRole('textbox')
+    expect(input.value).toBe('123')
+  })
+
+  it('should show user input after clicking numbers and operations', () => {
+    render(<Calculator />)
+    
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+
+    const plus = screen.getByText('+')
+    fireEvent.click(plus)
+    fireEvent.click(one)
+
+    const input = screen.getByRole('textbox')
+    expect(input.value).toBe('1+1')
+  })
+
+  it('should calculate based on user input and show the calculation', () => {
+    render(<Calculator />)
+    
+    const one = screen.getByText('1')
+    fireEvent.click(one)
+
+    const plus = screen.getByText('+')
+    fireEvent.click(plus)
+    fireEvent.click(one)
+
+    const equal = screen.getByText('=')
+    fireEvent.click(equal)
+
+    const input = screen.getByRole('textbox')
+    expect(input.value).toBe('2')
+  })
+
 })
